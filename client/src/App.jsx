458ad6cc081c6005c3a39cd2ac94a5fc6c781095
Shivansh Agrawal <shivansh.agrprof@gmail.com>
2025-03-01 19:21:08 +0530

    remove unused assets and components to clean up the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,21 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-import Home from './pages/home/Home';
-import Layout from './layout/Layout';
-import AuthLayout from './layout/AuthLayout';
 import './App.scss';
-import Video from './pages/video/Video';
-import Register from './pages/register/Register';
-import Login from './pages/login/Login';
 import store from './store';
 import {Provider} from 'react-redux'
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <>
-        <Route path='/' element={<Layout />}>
-          <Route path='' element={<Home />} />
-          <Route path='videos/:videoId' element={<Video />} />
-        </Route>
-        <Route path='/' element={<AuthLayout />}>
-          <Route path='register' element={<Register />} />
-          <Route path='login' element={<Login />} />
-        </Route>
-      </>
+      // <>
+      //   <Route path='/' element={<Layout />}>
+      //     <Route path='' element={<Home />} />
+      //     <Route path='videos/:videoId' element={<Video />} />
+      //   </Route>
+      //   <Route path='/' element={<AuthLayout />}>
+      //     <Route path='register' element={<Register />} />
+      //     <Route path='login' element={<Login />} />
+      //   </Route>
+      // </>
     )
   );
 
